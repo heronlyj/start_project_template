394d66d43d1f13ff6610b1a582962824132e61b0
heronlyj <heronlyj@gmail.com>
2021-12-30 16:40:09 +0800

    feat(*): flutter module 的形式集成 flutter_boost

--- App/App/AppDelegate.swift
+++ App/App/AppDelegate.swift
@@ -7,24 +7,28 @@
 
 import UIKit
 
+import Flutter
+import flutter_boost
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let rootNavigationController = UINavigationController(rootViewController: ViewController())
+        rootNavigationController.navigationBar.isHidden = true
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootNavigationController
+        window?.makeKeyAndVisible()
+        
+        let boostDelegate = BoostDelegate()
+        boostDelegate.navigationController = rootNavigationController
+        
+        FlutterBoost.instance().setup(application, delegate: boostDelegate) { engine in }
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
