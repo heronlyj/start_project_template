533a320bb735948ec851014d152a2695ac2bbd83
heronlyj <heronlyj@gmail.com>
2021-12-30 17:16:57 +0800

    feat(*): module 形式集成 flutter

--- App/App/ViewController.swift
+++ App/App/ViewController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+import Flutter
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toFlutter() {
+        if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+            let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+            self.present(flutterViewController, animated: true, completion: nil)
+        }
     }
 }
